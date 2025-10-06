a53c5511de07eacd3e61be0a7d2eae223f31ce9d
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2025-10-06 09:16:51 +0300

    Mitme avaldisega funktsioon on lisatud

--- funksioonid.sql
+++ funksioonid.sql
@@ -1,4 +1,5 @@
 --funksioonid
+use AdventureWorksDW2019
 select * from DimEmployee
 
 --Tabelisisev‰‰rtusega funktsioon e Inline Table Valued function (ILTVF)
@@ -10,3 +11,16 @@ from dbo.DimEmployee)
 
 --k‰ivita funksiooni
 select * from fn_ILTVF_GetEmployees();
+
+--Mitme avaldisega tabeliv‰‰rtusega funktsioonid e
+--multi-statement table valued function (MSTVF)
+create function fn_MSTVF_GetEmployees()
+returns @Table Table (EmployeeKey int, FirstName nvarchar(20), DOB Date)
+as
+Begin
+insert into @Table
+select EmployeeKey, FirstName, Cast(BirthDate as Date)
+from dbo.DimEmployee
+return 
+end
+select * from  fn_MSTVF_GetEmployees();
