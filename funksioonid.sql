651edce8a456a99c5a87fa159ce7bf49a19f000b
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2025-10-06 10:14:11 +0300

    Kohalike ja ajutiste tabelite lisamine

--- funksioonid.sql
+++ funksioonid.sql
@@ -56,3 +56,32 @@ return (Select FirstName from dbo.DimEmployee where EmployeeKey=@EmployeeKey)
 end
 
 drop table dbo.DimEmployee
+
+use AdventureWorksDW2019
+--Local tabeli loomine
+create table #PersonDetails(Id int, Name nvarchar(20))
+insert into #PersonDetails values(1, 'Mike')
+insert into #PersonDetails values (2, 'John')
+insert into #PersonDetails values (3, 'Todd')
+select * from #PersonDetails
+
+--Käsk tabeli kohalikkuse näitamiseks
+Select name from tempdb..sysobjects
+where name like '#PersonDetails%'
+
+--tabeli kustutamine
+drop table #PersonDetails
+
+--protseduuri loomine kohaliku tabeli loomiseks
+Create procedure spCreateLocalTempTable
+as
+begin
+Create table #PersonDetails(Id int, Name nvarchar(20))
+insert into #PersonDetails values(1, 'Mike')
+insert into #PersonDetails values (2, 'John')
+insert into #PersonDetails values (3, 'Todd')
+select * from #PersonDetails
+end
+
+--globaalse ajutise tabeli loomine
+create table ##EmployeeDetails (Id int, Name nvarchar(20))
